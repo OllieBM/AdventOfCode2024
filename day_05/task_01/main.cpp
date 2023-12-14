876376f91328153d34b797154383fed21dd82e10
olliebm <olliebm@live.co.uk>
2023-12-14 11:07:31 +0700

    fixed using mapping and range

--- day_05/task_01/main.cpp
+++ day_05/task_01/main.cpp
@@ -10,33 +10,19 @@
 #include <vector>
 using namespace std;
 
-map<unsigned long long, unsigned long long> add_to_map(std::string text) {
-  map<unsigned long long, unsigned long long> m{};
-  auto nums = std::regex("([0-9]+)");
-  std::vector<unsigned long long> vs{};
-  for (std::smatch sm; std::regex_search(text, sm, nums);) {
-    vs.push_back(stoull(text.c_str()));
-    text = sm.suffix();
-  }
-
-  auto source = vs[0];
-  auto destination = vs[0];
-  auto range = vs[0];
-  for (unsigned long long i = 0; i < range; i++) {
-    m.insert({source + i, destination + i});
-  }
-  return m;
-}
-
-void ParseInput(
-    string filename, vector<unsigned long long> &seeds,
-    map<unsigned long long, unsigned long long> &seed_to_soil,
-    map<unsigned long long, unsigned long long> &soil_to_fertilizer,
-    map<unsigned long long, unsigned long long> &fertilizer_to_water,
-    map<unsigned long long, unsigned long long> &water_to_light,
-    map<unsigned long long, unsigned long long> &light_to_temperature,
-    map<unsigned long long, unsigned long long> &temperature_to_humidity,
-    map<unsigned long long, unsigned long long> &humidity_to_location) {
+struct Mapping {
+  unsigned long long source;
+  unsigned long long destination;
+  unsigned long long range;
+};
+void ParseInput(string filename, vector<unsigned long long> &seeds,
+                vector<Mapping> &seed_to_soil,
+                vector<Mapping> &soil_to_fertilizer,
+                vector<Mapping> &fertilizer_to_water,
+                vector<Mapping> &water_to_light,
+                vector<Mapping> &light_to_temperature,
+                vector<Mapping> &temperature_to_humidity,
+                vector<Mapping> &humidity_to_location) {
 
   auto ifs = ifstream(filename);
   if (!ifs.is_open()) {
@@ -49,8 +35,7 @@ void ParseInput(
   string input_str = buffer.str();
   auto pos = size_t{0};
 
-  auto add_to_map = [](std::string text,
-                       map<unsigned long long, unsigned long long> &m) {
+  auto add_mapping = [](std::string text, vector<Mapping> &m) {
     auto nums = std::regex("([0-9]+)");
     std::vector<unsigned long long> vs{};
     for (std::smatch sm; std::regex_search(text, sm, nums);) {
@@ -59,12 +44,11 @@ void ParseInput(
     }
 
     for (auto i = size_t{0}; i < vs.size(); i += 3) {
-      auto destination = vs[i];
-      auto source = vs[i + 1];
-      auto range = vs[i + 2];
-      for (unsigned long long j = 0; j < range; j++) {
-        m.insert({source + j, destination + j});
-      }
+      Mapping mapping;
+      mapping.destination = vs[i];
+      mapping.source = vs[i + 1];
+      mapping.range = vs[i + 2];
+      m.push_back(mapping);
     }
   };
 
@@ -85,19 +69,19 @@ void ParseInput(
 
     } else if (s.rfind("seed-to-soil", 0) != string::npos) {
       // std::cout << "add to map : seed-to-soil" << endl;
-      add_to_map(s, ref(seed_to_soil));
+      add_mapping(s, ref(seed_to_soil));
     } else if (s.rfind("soil-to-fertilizer", 0) != string::npos) {
-      add_to_map(s, ref(soil_to_fertilizer));
+      add_mapping(s, ref(soil_to_fertilizer));
     } else if (s.rfind("fertilizer-to-water", 0) != string::npos) {
-      add_to_map(s, ref(fertilizer_to_water));
+      add_mapping(s, ref(fertilizer_to_water));
     } else if (s.rfind("water-to-light", 0) != string::npos) {
-      add_to_map(s, ref(water_to_light));
+      add_mapping(s, ref(water_to_light));
     } else if (s.rfind("light-to-temperature", 0) != string::npos) {
-      add_to_map(s, ref(light_to_temperature));
+      add_mapping(s, ref(light_to_temperature));
     } else if (s.rfind("temperature-to-humidity", 0) != string::npos) {
-      add_to_map(s, ref(temperature_to_humidity));
+      add_mapping(s, ref(temperature_to_humidity));
     } else if (s.rfind("humidity-to-location", 0) != string::npos) {
-      add_to_map(s, ref(humidity_to_location));
+      add_mapping(s, ref(humidity_to_location));
     } else {
       cout << "unkown " << s << "\n";
     }
@@ -109,48 +93,58 @@ void ParseInput(
 int main(void) {
 
   vector<unsigned long long> seeds{};
-  map<unsigned long long, unsigned long long> seed_to_soil{};
-  map<unsigned long long, unsigned long long> soil_to_fertilizer{};
-  map<unsigned long long, unsigned long long> fertilizer_to_water{};
-  map<unsigned long long, unsigned long long> water_to_light{};
-  map<unsigned long long, unsigned long long> light_to_temperature{};
-  map<unsigned long long, unsigned long long> temperature_to_humidity{};
-  map<unsigned long long, unsigned long long> humidity_to_location{};
+  vector<Mapping> seed_to_soil{};
+  vector<Mapping> soil_to_fertilizer{};
+  vector<Mapping> fertilizer_to_water{};
+  vector<Mapping> water_to_light{};
+  vector<Mapping> light_to_temperature{};
+  vector<Mapping> temperature_to_humidity{};
+  vector<Mapping> humidity_to_location{};
   ParseInput("input.txt", seeds, seed_to_soil, soil_to_fertilizer,
              fertilizer_to_water, water_to_light, light_to_temperature,
              temperature_to_humidity, humidity_to_location);
 
-  auto find_in_map_or_default =
-      [](unsigned long long seed,
-         std::map<unsigned long long, unsigned long long> m) {
-        auto it = m.find(seed);
-        if (it != m.end()) {
-          return it->second;
-        }
-        return seed;
-      };
+  auto find_value_from_mapping = [](unsigned long long source,
+                                    const std::vector<Mapping> &mapping) {
+    // cout << "source " << source << endl;
 
+    for (auto i = size_t{0}; i < mapping.size(); i++) {
+      auto m = mapping[i];
+      // cout << "mapping.source " << m.source << endl;
+      // cout << "mapping.destination " << m.destination << endl;
+      // cout << "mapping.range " << m.range << endl;
+      auto v = source - m.source;
+      if (v < 0) {
+        continue;
+      }
+      if (v <= m.range) {
+        return m.destination + v;
+      }
+    }
+    // if there is no mapping assume it is the same
+    return source;
+  };
   auto min_location = numeric_limits<unsigned long long>::max();
   for (auto i : seeds) {
-    std::cout << "seed " << i;
-    auto soil = find_in_map_or_default(i, seed_to_soil);
-    auto fertilizer = find_in_map_or_default(soil, soil_to_fertilizer);
-    auto water = find_in_map_or_default(fertilizer, fertilizer_to_water);
-    auto light = find_in_map_or_default(water, water_to_light);
-    auto temperature = find_in_map_or_default(light, light_to_temperature);
+    // std::cout << "seed " << i;
+    auto soil = find_value_from_mapping(i, seed_to_soil);
+    auto fertilizer = find_value_from_mapping(soil, soil_to_fertilizer);
+    auto water = find_value_from_mapping(fertilizer, fertilizer_to_water);
+    auto light = find_value_from_mapping(water, water_to_light);
+    auto temperature = find_value_from_mapping(light, light_to_temperature);
     auto humidity =
-        find_in_map_or_default(temperature, temperature_to_humidity);
-    auto location = find_in_map_or_default(humidity, humidity_to_location);
+        find_value_from_mapping(temperature, temperature_to_humidity);
+    auto location = find_value_from_mapping(humidity, humidity_to_location);
 
-    std::cout << " location " << location << std::endl;
-    // cout << "seed [" << i << "] ";
-    // cout << "soil " << soil << " ";
-    // cout << "fertilizer " << fertilizer << " ";
-    // cout << "water " << water << " ";
-    // cout << "light " << light << " ";
-    // cout << "temperature " << temperature << " ";
-    // cout << "humidity " << humidity << " ";
-    // cout << "location " << location << "\n";
+    // std::cout << " location " << location << std::endl;
+    cout << "seed [" << i << "] ";
+    cout << "soil " << soil << " ";
+    cout << "fertilizer " << fertilizer << " ";
+    cout << "water " << water << " ";
+    cout << "light " << light << " ";
+    cout << "temperature " << temperature << " ";
+    cout << "humidity " << humidity << " ";
+    cout << "location " << location << "\n";
     min_location = min(location, min_location);
   }
   std::cout << min_location << std::endl;
